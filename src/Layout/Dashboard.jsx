f4609c3dc71e86650ccfa22a4d5ab2ef0e98456c
asadullahislam <asadullah0072002@gmail.com>
2024-03-06 15:34:07 +0600

    done

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -14,7 +14,7 @@ const Dashboard = () => {
     return (
         <div className="flex">
             {/* dashboard side bar */}
-            <div className="w-64 min-h-screen  bg-orange-400">
+            <div className="w-64 min-h-screen sm:w-1/4  bg-orange-400">
                 <ul className="menu p-4">
                     {
                         isAdmin ? <>
